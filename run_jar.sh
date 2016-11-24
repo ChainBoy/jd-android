cd25639a39e903fd7dcf0be7e339171b1b4f8744
zhipeng <qq1126918228@gmail.com>
2016-11-24 12:11:35 +0800

    TODO: support web method crawler.

--- run_jar.sh
+++ run_jar.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh
 path="/sdcard/test"
 export CLASSPATH=$path/Root.jar
-app_process32 $path com.jingdong.app.Root --alltype&
+app_process32 $path com.jingdong.app.Root --alltype --setall &
+#app_process32 $path com.jingdong.app.Root --alltype --setall --debug&
 #app_process32 $path com.jingdong.app.Root --debug --alltype&
